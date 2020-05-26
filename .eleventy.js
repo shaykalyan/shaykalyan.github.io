92a614cf343cc6e3c25b85b647b890e94c18b56c
Shay Kalyan <akshaykalyan@outlook.com>
2020-05-25 19:26:30 -0700

    work in progress!

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const moment = require('moment');
+
+moment.locale('en');
+
+
+module.exports = function (eleventyConfig) {
+  // eleventyConfig.setTemplateFormats([
+  //   "md",
+  // ]);
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    // Optional, default is "---"
+    //excerpt_separator: "<!-- excerpt -->"
+  });
+
+
+  return {
+    dir: {
+      input: "./eleventy/",
+      output: "./blog/"
+    },
+    pathPrefix: "/blog/"
+    // templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+  }
+};
---
+++ eleventy/posts/2020/first-post.md
@@ -0,0 +1,20 @@
+---
+layout: _layouts/post.njk
+tags: ['post', 'meta']
+title: Welcome to the blog
+date: 2019-05-15
+draft: true
+---
+my exerpt
+---
+
+### Sub sub titletest
+
+Eiusmod voluptate enim reprehenderit sint culpa aliquip laborum reprehenderit cillum. Elit dolor in cupidatat adipisicing nulla velit et. Dolor laborum dolor enim labore exercitation ut. Elit sit et adipisicing proident elit Lorem quis ut laborum adipisicing. Minim nostrud in tempor velit.
+
+Commodo nisi nostrud culpa nisi. Ut nulla dolore ipsum quis non enim cupidatat et ad. Eiusmod qui eu magna velit sunt fugiat duis eiusmod ut irure commodo laborum proident.
+
+```
+code block?
+```
+
