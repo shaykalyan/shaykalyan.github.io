cf462cb1e8eca2d2f82647fa9d6017cb24b21261
Shay Kalyan <akshaykalyan@outlook.com>
2026-03-08 09:42:11 -0700

    refactor(frontend): remove jQuery dependency from site navigation scripts

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
-$(document).ready(function($) {
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.getElementById("nav");
+  const navTrigger = document.querySelector(".nav-trigger");
 
-	// responsive nav
-	$(".nav-trigger").on('click', function(event) {
-		event.preventDefault();
-		$('#nav').toggleClass('nav-open');
-		$(this).toggleClass('nav-open');
-	});
+  if (nav && navTrigger) {
+    navTrigger.addEventListener("click", (event) => {
+      event.preventDefault();
+      nav.classList.toggle("nav-open");
+      navTrigger.classList.toggle("nav-open");
+    });
 
-	$("#nav a").on('click', function(event) {
-		$('#nav').toggleClass('nav-open');
-		$('.nav-trigger').toggleClass('nav-open');
-	});
+    nav.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        nav.classList.remove("nav-open");
+        navTrigger.classList.remove("nav-open");
+      });
+    });
+  }
 
-	// smooth scroll to div - http://css-tricks.com/snippets/jquery/smooth-scrolling/
-	$(function() {
-		$('a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop : target.offset().top
-					}, 700);
-					return false;
-				}
-			}
-		});
-	});
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (event) => {
+      const href = anchor.getAttribute("href");
+      if (!href || href === "#" || href === "#0") {
+        return;
+      }
+
+      const target = document.querySelector(href);
+      if (!target) {
+        return;
+      }
+
+      event.preventDefault();
+      target.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  });
 });
-
-
